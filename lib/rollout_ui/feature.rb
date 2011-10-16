eb8a6f3041533a5781436218bbed680c94ae4447
John Allison <jrallison@gmail.com>
2011-10-16 00:06:57 -0400

    Delegate redis keys to the rollout instance

--- lib/rollout_ui/feature.rb
+++ lib/rollout_ui/feature.rb
@@ -4,21 +4,23 @@ module RolloutUi
 
     attr_reader :name
 
+    delegate :percentage_key, :group_key, :user_key, :to => :rollout
+
     def initialize(name)
       @wrapper = Wrapper.new
       @name = name
     end
 
     def percentage
-      redis.get("feature:#{name}:percentage")
+      redis.get(percentage_key(name))
     end
 
     def groups
-      redis.smembers("feature:#{name}:groups")
+      redis.smembers(group_key(name))
     end
 
     def users
-      redis.smembers("feature:#{name}:users")
+      redis.smembers(user_key(name))
     end
 
     def percentage=(percentage)
@@ -26,12 +28,12 @@ module RolloutUi
     end
 
     def groups=(groups)
-      redis.del("feature:#{name}:groups")
+      redis.del(group_key(name))
       groups.each { |group| rollout.activate_group(name, group) }
     end
 
     def users=(users)
-      redis.del("feature:#{name}:users")
+      redis.del(user_key(name))
       users.each { |user| rollout.activate_user(name, User.new(user)) unless user.empty? }
     end
 
