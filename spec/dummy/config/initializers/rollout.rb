6de6266d2be357678677748d0fc7de9a8c521aaf
John Allison <jrallison@gmail.com>
2011-10-17 00:19:20 -0400

    Ajaxify feature form

--- spec/dummy/config/initializers/rollout.rb
+++ spec/dummy/config/initializers/rollout.rb
@@ -1,3 +1,6 @@
 $redis = Redis.new
 $rollout = Rollout.new($redis)
 RolloutUi.wrap($rollout)
+
+$rollout.define_group(:admins) { |user| user.admin? }
+$rollout.define_group(:beta_testers) { |user| user.beta_testers? }
