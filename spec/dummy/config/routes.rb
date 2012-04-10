637809681899536f5188fddd3bb62f4f1904c281
Stefano B <stefano.ballabeni+github@gmail.com>
2012-04-10 13:39:47 -0400

    specify rolloutui routes proxy name, not needed but a good practice in my opinion

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RolloutUi::Engine => "/rollout"
+  mount RolloutUi::Engine => "/rollout", :as => 'rollout'
 
   root :to => proc { |env| [200, {}, "Hello World!"] }
 end
