d5fa2d9ed7f94910399c033ac7263551cfa9ce2e
Ches Martin <ches@whiskeyandgrits.net>
2012-10-12 01:02:54 +0700

    Fix monkey patch for Rollout 1.2.0
    The latest Rollout makes the second argument of `active?` optional -- if
    nil it checks if a feature is globally active. Adding the default to the
    monkey patch should be backwards compatible with older versions of
    Rollout for non-pathological cases since there is no arity change.

--- lib/rollout_ui/monkey_patch.rb
+++ lib/rollout_ui/monkey_patch.rb
@@ -1,7 +1,7 @@
 class Rollout
   alias_method :original_active?, :active?
 
-  def active?(feature, user)
+  def active?(feature, user=nil)
     RolloutUi::Wrapper.new(self).add_feature(feature)
     original_active?(feature, user)
   end
